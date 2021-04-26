f023ffbaf5398dd2fa946d3fc80c5d60768bba4e
AkmalDykaB <akmaldykab@student.telkomuniversity.ac.id>
2021-04-26 22:40:37 +0700

    Menyimpan data ke database

--- app/src/main/java/org/d3if4076/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4076/hitungbmi/ui/hitung/HitungFragment.kt
@@ -3,20 +3,27 @@ package org.d3if4076.hitungbmi.ui.hitung
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import org.d3if4076.hitungbmi.R
 import org.d3if4076.hitungbmi.data.KategoriBmi
 import org.d3if4076.hitungbmi.databinding.FragmentHitungBinding
+import org.d3if4076.hitungbmi.db.BmiDb
 import org.d3if4076.hitungbmi.ui.HitungFragmentDirections
 
 class HitungFragment : Fragment() {
     private lateinit var binding: FragmentHitungBinding
-    private val viewModel: HitungViewModel by viewModels()
+    private val viewModel: HitungViewModel by lazy {
+        val db = BmiDb.getInstance(requireContext())
+        val factory = HitungViewModelFactory(db.dao)
+        ViewModelProvider(this, factory).get(HitungViewModel::class.java)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -60,6 +67,11 @@ class HitungFragment : Fragment() {
             binding.kategoriTextView.text = getString(R.string.kategori_x,
                     getKategori(it.kategori))
             binding.buttonGroup.visibility = View.VISIBLE
+        })
+
+        viewModel.data.observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            Log.d("HitungFragment", "Data tersimpan. ID = ${it.id}")
         })
     }
 
